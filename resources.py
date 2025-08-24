e4f5116b6d9e426d23ea01643e96bf8cf4dfb5b7
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-08-24 21:51:44 +0300

    correction made to data transform asset

--- resources.py
+++ resources.py
@@ -5,5 +5,9 @@ from dagster_duckdb import DuckDBResource
 database_resource = DuckDBResource(database="./data/staging/transformed_data.duckdb")
 
 
+@dg.definitions
+def resources():
+     return dg.Definitions(resources={"database": database_resource})
+
 
 
