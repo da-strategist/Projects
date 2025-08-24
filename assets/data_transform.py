5453e224cb26b21f1e4e4d43b96820e9e04731c3
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-08-24 22:13:03 +0300

    transformed data asset corrected

--- assets/data_transform.py
+++ assets/data_transform.py
@@ -5,18 +5,29 @@ from dagster_duckdb import DuckDBResource
 
 
 
-@dg.asset(
-        deps=["data_ingestion"],
-        required_resource_keys={"database"},
-          )
-def transform_data(context, data_ingestion) -> None:
 
-    conn = context.resources.database
-    conn.register("retail_data_processed", data_ingestion)
-    conn.execute(
-        """
-        create or replace table retail_data_processed as
-        SELECT * FROM my_data
-        """
-    )
-    conn.close()
+@dg.asset(
+    deps=["data_ingestion"],
+    required_resource_keys={"database"},
+)
+def transform_data(context, data_ingestion) -> None:
+    with context.resources.database.get_connection() as conn:
+        conn.register("my_data", data_ingestion)
+        conn.execute("""
+            create or replace table retail_data_processed as
+            select * from my_data
+        """)
+
+
+
+
+@dg.asset(
+    deps=["data_ingestion"],
+)
+def transformed_data(database: DuckDBResource) -> None:
+    query = """
+        CREATE OR REPLACE TABLE processed_retail_data AS
+        SELECT * FROM retail_raw
+    """
+    with database.get_connection() as conn:
+        conn.execute(query)
